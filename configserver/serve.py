58f1308892451a2285095cd0c6329f017b9b7184
C5H12O5 <jam.xylitol@gmail.com>
2023-08-16 17:08:25 +0800

    Save configuration file to local system

--- configserver/serve.py
+++ configserver/serve.py
@@ -9,16 +9,16 @@ from pathlib import Path
 _host = "0.0.0.0"
 _port = 5125
 _index_html = ""
+_basedir = Path(__file__).resolve().parent
 
 
 def init_server():
     """Initialize the server."""
     global _index_html
-    basedir = Path(__file__).resolve().parent
 
     # get list of sites and types from flow definitions
     sites = {}
-    for filepath in (basedir / "../scrapeflows").glob("*.json"):
+    for filepath in (_basedir / "../scrapeflows").glob("*.json"):
         with open(filepath, "r", encoding="utf-8") as flowdef_json:
             flowdef = json.load(flowdef_json)
             site = flowdef["site"]
@@ -30,7 +30,7 @@ def init_server():
 
     # generate HTML for source list
     source_html = ""
-    with open(basedir / "templates/source.html", "r", encoding="utf-8") as html:
+    with open(_basedir / "templates/source.html", "r", encoding="utf-8") as html:
         source_tmpl = string.Template(html.read())
     for site, types in sites.items():
         movie = "selected" if "movie" in types else "disabled"
@@ -39,7 +39,7 @@ def init_server():
         source_html += source_tmpl.substitute(source)
 
     # generate HTML for index page
-    with open(basedir / "templates/index.html", "r", encoding="utf-8") as html:
+    with open(_basedir / "templates/index.html", "r", encoding="utf-8") as html:
         index_tmpl = string.Template(html.read())
         _index_html = index_tmpl.substitute(sources=source_html)
 
@@ -67,7 +67,8 @@ class RequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers["Content-Length"])
         body = self.rfile.read(content_length)
-        print(body)
+        with open(_basedir / "../scrapeflows.conf", "w", encoding="utf-8") as w:
+            w.write(body.decode("utf-8"))
         self.send_response(200)
         self.end_headers()
 
